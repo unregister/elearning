ab5a13e1912cf5fc0b2881e99100582a0fd70964
Nanang <haries@jogjacamp.co.id>
2014-02-21 19:41:19 +0700

    pannel menu

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,3 +1,12 @@
-<div class="col-md-3">
-sidebar
+<div class="col-md-3" id="sidebar">
+
+    <div class="panel panel-primary">
+    
+        <div class="panel-heading"><h3 class="panel-title">Panel title</h3></div>
+        <div class="panel-body">
+            Panel content
+        </div>
+    
+    </div>
+
 </div>
--- modules/home/home.php
+++ modules/home/home.php
@@ -1,3 +1,10 @@
-<div class="col-md-9">
-Home
+<div class="col-md-9" id="content-container">
+<div class="panel panel-primary">
+    
+        <div class="panel-heading"><h3 class="panel-title">Content</h3></div>
+        <div class="panel-body">
+        Content
+        </div>
+    
+    </div>
 </div>
